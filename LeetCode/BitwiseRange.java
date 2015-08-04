d9449decca3cbb59398f7c65480639d6799b6e47
tsh3lley <thomasjshelley@gmail.com>
2015-08-04 15:50:10 +0800

    used idea from forum to fix time complexity

--- LeetCode/BitwiseRange.java
+++ LeetCode/BitwiseRange.java
@@ -1,16 +1,14 @@
 
 public class BitwiseRange {
 	public static void main(String args[]){
-		System.out.println(rangeBitwiseAnd(0,7)); //4
+		System.out.println(rangeBitwiseAnd(6,7)); //4
 		System.out.println((5&6)&7);
 		
 	}
 	public static int rangeBitwiseAnd(int m, int n) {
 		if (m==0) return 0;
-		int total = m;
-        for (int i=m+1;i<=n;i++){
-        	total &= i;
-        }
-        return total;
+		int r=Integer.MAX_VALUE;
+	    while((m&r)!=(n&r))  r=r<<1;
+	    return n&r;
     }
 }
