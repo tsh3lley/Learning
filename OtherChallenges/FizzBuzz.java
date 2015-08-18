f034f17d07eadb1a967596a8b045ec65035ed3d4
tsh3lley <thomasjshelley@gmail.com>
2015-08-19 05:35:13 +0800

    added fizzbuzz lol

---
+++ OtherChallenges/FizzBuzz.java
@@ -0,0 +1,25 @@
+
+public class FizzBuzz {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<101;i++){
+			if(i%3==0 && i%5 != 0){
+				System.out.println("Fizz");
+			
+			}
+			else if(i%5==0 && i%3 != 0){
+				System.out.println("Buzz");
+			
+			}
+			else if(i%3==0 && i%5 == 0){
+				System.out.println("FizzBuzz");
+			}
+			else{
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
